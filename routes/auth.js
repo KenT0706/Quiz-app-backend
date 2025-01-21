25a8f8cef9d2ac376404b5fd371141c675b5c5d4
Ken <kentengkt@outlook.com>
2025-01-21 22:56:09 +0800

    updated bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs'); // Replace bcrypt with bcryptjs
 const router = express.Router();
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
