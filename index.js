da484bb4a86b5285c5145ea7d7ed2eb5ea20b4bf
Ken <kentengkt@outlook.com>
2025-01-21 16:42:45 +0800

    updated index.js and added vercel.json

--- index.js
+++ index.js
@@ -6,19 +6,13 @@ require('dotenv').config();
 
 const app = express({ mergeParams: true });
 
-// Configure CORS to allow requests from your frontend
-app.use(cors({
-  origin: 'http://localhost:5173', // Replace with your frontend URL during development
-  methods: 'GET,POST,PUT,DELETE', // Specify the methods you want to allow
-  credentials: true, // Enable if you use cookies or other credentials
-}));
-
 // Configure middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('frontend/dist'));
 
 // Connect to MongoDB
-const mongoURI = process.env.MONGODB_URI || "your-mongo-uri";
+const mongoURI = process.env.MONGODB_URI || "mongodb+srv://kenktent:Short%40aa@kenblitz.gka5p.mongodb.net/QuizApp?retryWrites=true&w=majority";
 mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
